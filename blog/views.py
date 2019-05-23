9225469adf3ef2e4c918e4f557ca4e9bbab06bfa
Emmanuelle <emmanuelle.nelly.olioti@gmail.com>
2019-05-22 21:30:56 -0300

    primeiro projeto

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
-
+def projeto(request):
+    return render(request, 'blog/projeto.html', {})
